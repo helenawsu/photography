d9e1ad9a39c82e1ddd658a7f591e5507afe5ba67
HSWWWW <Helena1980938619@gmail.com>
2022-09-05 19:46:30 -0700

    (-.-) un-working mess

---
+++ src/create_img.js
@@ -0,0 +1,23 @@
+export const photos = [
+  {
+    title: 'Trekking',
+    path: './photos/IMG_2043.webp',
+    original_path: '',
+    tags: ['Landscape', 'Europe'],
+    time: [2022, 'July', ''],
+  },
+  {
+    title: 'Melting Origin',
+    path: './photos/IMG_2176.webp',
+    original_path: '',
+    tags: ['Landscape', 'Europe'],
+    time: [2022, 'July', ''],
+  },
+  {
+    title: 'A sheep meh like cow',
+    path: './photos/IMG_2214.webp',
+    original_path: '',
+    tags: ['Animal', 'Europe'],
+    time: [2022, 'July', ''],
+  },
+];
