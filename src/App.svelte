1404bcba9e72c727dc5a735f3fba4e8bf7a2edd6
Helena Su <helenasu@berkeley.edu>
2023-05-15 02:05:00 -0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -48,7 +48,7 @@
   {#if $show_full_img}
     <FullScreenImage />
   {:else}
-    <p class="lastupdatetime">This page was last updated on Nov 6th, 2022 with {num_of_pic} photos.</p>
+    <p class="lastupdatetime">This page was last updated on May 14th, 2023 with {num_of_pic} photos.</p>
     <h1 style="padding-bottom: 10px">Helena Su Photograhpy</h1>
 
     <br />
